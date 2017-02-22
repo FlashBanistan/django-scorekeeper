412fcaee8fb730cda683b255144af8c3f20c4795
Aaron <FlashBanistan66@gmail.com>
2017-02-21 21:42:57 -0700

    add Statistics model

--- books_and_run/admin.py
+++ books_and_run/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Game, Score
+from .models import Game, Score, Statistics
 
-admin.site.register([Game, Score])
+admin.site.register([Game, Score, Statistics])
---
+++ books_and_run/models/Statistics.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Statistics(models.Model):
+    # Define model fields:
+    user = models.OneToOneField(User)
+    games_won = models.IntegerField(null=True, blank=True)
+    hands_won = models.IntegerField(null=True, blank=True)
+    games_played = models.IntegerField(null=True, blank=True)
+    high_score = models.IntegerField(null=True, blank=True)
+    low_score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.pk)
