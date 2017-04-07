77234523d54e2d41201b0ac68afe4e6e539de64e
Aaron Chesley <achesley@crossroadstech.net>
2017-04-07 11:37:18 -0600

    add error handling to PUT action

--- books_and_run/api/serializers.py
+++ books_and_run/api/serializers.py
@@ -1,52 +1,6 @@
 from rest_framework import serializers
 from books_and_run.models import Statistics
-from accounts.api.serializers import UserDetailSerializer
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-
-from django.core.exceptions import ObjectDoesNotExist
-
-"""
-Statistics endpoints
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# class StatisticsCreateSerializer(ModelSerializer):
-#     class Meta:
-#         model = Statistics
-#         fields = [
-#             'user',
-#             'games_won',
-#             'hands_won',
-#             'games_played',
-#             'high_score',
-#             'low_score',
-#         ]
-#
-#
-# class StatisticsDetailSerializer(ModelSerializer):
-#     user = UserDetailSerializer(read_only=True)
-#     class Meta:
-#         model = Statistics
-#         fields = [
-#             'user',
-#             'games_won',
-#             'hands_won',
-#             'games_played',
-#             'high_score',
-#             'low_score',
-#         ]
-#
-# class StatisticsUpdateSerializer(ModelSerializer):
-#     class Meta:
-#         model = Statistics
-#         fields = [
-#             'games_won',
-#             'hands_won',
-#             'games_played',
-#             'high_score',
-#             'low_score',
-#         ]
 
 
 class StatisticsSerializer(serializers.ModelSerializer):
--- books_and_run/api/views.py
+++ books_and_run/api/views.py
@@ -1,55 +1,24 @@
+from scorekeeper.mixins import DefaultsMixin
 from .serializers import StatisticsSerializer
 from books_and_run.models import Statistics
-from rest_framework.filters import (
-    DjangoFilterBackend,
-    SearchFilter,
-    OrderingFilter,
-)
-from rest_framework.permissions import (
-    # AllowAny,
-    IsAuthenticated,
-    # IsAdminUser,
-    # IsAuthenticatedOrReadOnly,
-)
-from rest_framework.pagination import (
-    LimitOffsetPagination,
-    PageNumberPagination,
-)
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework import viewsets
 from rest_framework.response import Response
 import django_filters
-from rest_framework.decorators import detail_route
+from rest_framework.exceptions import ParseError
 
 
 
-"""
-Statistic views
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 class StatisticsFilter(django_filters.FilterSet):
 
     class Meta:
         model = Statistics
-        fields = ('games_won', 'hands_won', 'games_played', 'high_score', 'low_score')
-
-
-class DefaultsMixin(object):
-    """Default settings for view authentication, permissions, filtering and pagination."""
-
-    authentication_classes = (JSONWebTokenAuthentication, )
-
-    permission_classes = (
-        IsAuthenticated,
-    )
-
-    paginate_by = 25
-    paginate_by_param = 'page_size'
-    max_paginate_by = 100
-    filter_backends = (
-        DjangoFilterBackend,
-        SearchFilter,
-        OrderingFilter,
-    )
+        fields = (
+            'games_won',
+            'hands_won',
+            'games_played',
+            'high_score',
+            'low_score',
+        )
 
 
 class StatisticsViewSet(DefaultsMixin, viewsets.ModelViewSet):
@@ -57,18 +26,37 @@ class StatisticsViewSet(DefaultsMixin, viewsets.ModelViewSet):
     serializer_class = StatisticsSerializer
     filter_class = StatisticsFilter
     search_fields = ('pk', 'user')
-    ordering_fields = ('games_won', 'hands_won', 'games_played', 'high_score', 'low_score')
+    ordering_fields = (
+        'games_won',
+        'hands_won',
+        'games_played',
+        'high_score',
+        'low_score',
+    )
 
+    """
+    Override the http PUT action to accept and handle the following parameters:
+        'is_winner',
+        'num_hands_won',
+        'score'.
+    """
     def update(self, request, *args, **kwargs):
-        stats = self.get_object()
+        # Check the request for correct data:
+        try:
+            request.data['is_winner']
+            request.data['num_hands_won']
+            request.data['score']
+        except KeyError as e:
+            raise ParseError(detail="Missing key: " + str(e))
 
+        # Handle the request:
+        stats = self.get_object()
         stats.increment_games_won(request.data['is_winner'])
         stats.add_to_hands_won(request.data['num_hands_won'])
         stats.increment_games_played()
         stats.new_low_score(request.data['score'])
         stats.new_high_score(request.data['score'])
-
         stats.save()
-
         serialized_stats = StatisticsSerializer(stats, context={'request': request}).data
+        
         return Response(serialized_stats)
