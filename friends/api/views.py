2ced31f2e927f36b0448ca82de9b6c5c580d86e4
Aaron Chesley <achesley@crossroadstech.net>
2017-05-02 14:37:06 -0600

    add search url

--- friends/api/views.py
+++ friends/api/views.py
@@ -1,11 +1,31 @@
 from .serializers import FriendListDetailSerializer
+from users.api.serializers import UserDetailSerializer
 from ..models import FriendList
 from rest_framework import viewsets
 from scorekeeper.mixins import DefaultsMixin
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+import django_filters
 
-
+class FriendFilter(django_filters.FilterSet):
+    class Meta:
+        model = FriendList
+        fields = (
+            'friends__username',
+        )
 
 class FriendListViewSet(DefaultsMixin, viewsets.ModelViewSet):
     queryset = FriendList.objects.all()
     serializer_class = FriendListDetailSerializer
-    # filter_class = asdf
+    filter_class = FriendFilter
+
+    @detail_route(methods=['get'])
+    def get_friend(self, request, pk=None):
+        # print(dir(request.query_params))
+        # print(request.query_params)
+        queryset = self.get_queryset().get(pk=pk)
+        friend = queryset.friends.get(username__icontains=request.query_params.get('username'))
+        serialized_friend = UserDetailSerializer(friend, context={'request': request}).data
+
+
+        return Response(serialized_friend)
